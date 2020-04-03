d8c69ddda5e5ea9e7197f4751647b44030c7cba8
artur.reinke <artur.reinke@logon.pl>
2020-04-03 17:33:50 +0200

    commit

---
+++ src/artur/reinke/Main.java
@@ -0,0 +1,19 @@
+package artur.reinke;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        i = count(i);
+
+        System.out.println(i);
+    }
+
+        public static int count(int value)
+        {
+            value = 101;
+            return value;
+        }
+
+}
